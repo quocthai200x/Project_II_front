ac8fa4baa7c11217f41ca49f3c8170d70258e2f8
Quoc Thai <quocthai2000x@gmail.com>
2022-07-04 00:38:26 +0700

    fix bug image size

--- src/pages/HomePage/MatchChatCol/MatchList.jsx
+++ src/pages/HomePage/MatchChatCol/MatchList.jsx
@@ -9,7 +9,7 @@ function MatchList({ listMatch, onClickMatchItem }) {
                 return (
                     <li onClick={() => onClickMatchItem(item)} key={'match-list-' + index} className="m-2 relative hover:scale-105 cursor-pointer transition hover:opacity-80" >
                         <span className='absolute bottom-2 left-2 text-xs font-black text-zinc-200'> {item.info.name}</span>
-                        <img loading='lazy' className='bg-cover rounded-xl aspect-[4/5] ' src={item.info.imgUrl} alt="" />
+                        <img loading='lazy' className='w-full bg-cover rounded-xl aspect-[4/5] ' src={item.info.imgUrl} alt="" />
                     </li>
                 )
             })}
