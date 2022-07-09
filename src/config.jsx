f9abb2dad7a800756e094d572f7cb24374fb15f0
Quoc Thai <quocthai2000x@gmail.com>
2022-07-09 22:11:15 +0700

    change url back in product env

--- src/config.jsx
+++ src/config.jsx
@@ -1,5 +1,5 @@
 export default {
     rootPath: process.env.NODE_ENV === 'development'
-    ?'http://localhost:5000':'http://localhost:5000'
+    ?'http://localhost:5000':'https://project-ii-back.herokuapp.com/'
     // rootPath: 'http://localhost:6969'
 };
